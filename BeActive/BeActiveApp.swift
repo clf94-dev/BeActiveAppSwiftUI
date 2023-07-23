8a815f4b0923daf46072e10c8ac7925279ba94ec
clf94-dev <clf94@hotmail.es>
2023-07-23 15:08:23 +0200

    Initial Commit

---
+++ BeActive/BeActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeActiveApp.swift
+//  BeActive
+//
+//  Created by Carmen Lucas on 23/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BeActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
