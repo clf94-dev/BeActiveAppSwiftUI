ee695338d04f5151f1974e17bbd0d88a1cb631bc
clf94-dev <clf94@hotmail.es>
2023-07-23 21:44:23 +0200

    - show coreTraining week data - show trainings weekly calories burned

--- BeActive/HealthManager.swift
+++ BeActive/HealthManager.swift
@@ -56,6 +56,7 @@ class HealthManager : ObservableObject {
                     fetchTodayCalories()
                     fetchWeekStrengthStats()
                     fetchWeekRowingStats()
+                    fetchWeekCoreStats()
                     
                 } catch {
                     print("error fetching the health data")
@@ -105,20 +106,26 @@ class HealthManager : ObservableObject {
         let timePredicate = HKQuery.predicateForSamples(withStart: .startOfWeek, end: Date())
         let workoutPredicate = HKQuery.predicateForWorkouts(with: .functionalStrengthTraining)
         let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [timePredicate, workoutPredicate])
-        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 10, sortDescriptors: nil) { _, sample, error in
+        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 20, sortDescriptors: nil) { _, sample, error in
             guard let workouts = sample as? [HKWorkout], error == nil else {
                 print("error fetching week strength training data")
                 return
             }
             var count: Int = 0
+            var countEnergy: Int = 0
             for workout in workouts {
                 let duration = Int(workout.duration)/60
                 count += duration
+                countEnergy += Int(workout.totalEnergyBurned!.doubleValue(for: .kilocalorie()))
                 
             }
             let activity = Activity(id: 2, title: "Strength", subtitle: "This week", amount: "\(count) minutes", image: "figure.strengthtraining.functional")
             DispatchQueue.main.async{
-                self.activities["weekRunning"] = activity
+                self.activities["weekStrength"] = activity
+            }
+            let activityEnergy = Activity(id: 3, title: "Strength", subtitle: "Calories this week", amount: "\(countEnergy) kcal", image: "figure.strengthtraining.functional")
+            DispatchQueue.main.async{
+                self.activities["weekStrengthEnergy"] = activityEnergy
             }
             
         }
@@ -129,23 +136,60 @@ class HealthManager : ObservableObject {
         let timePredicate = HKQuery.predicateForSamples(withStart: .startOfWeek, end: Date())
         let workoutPredicate = HKQuery.predicateForWorkouts(with: .rowing)
         let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [timePredicate, workoutPredicate])
-        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 10, sortDescriptors: nil) { _, sample, error in
+        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 20, sortDescriptors: nil) { _, sample, error in
             guard let workouts = sample as? [HKWorkout], error == nil else {
                 print("error fetching week rowing training data")
                 return
             }
             var count: Int = 0
+            var countEnergy: Int = 0
             for workout in workouts {
                 print(workout.duration)
                 print(workout.sampleType)
+                print(workout.totalEnergyBurned)
 
+                
                 let duration = Int(workout.duration)/60
                 count += duration
+                countEnergy += Int(workout.totalEnergyBurned!.doubleValue(for: .kilocalorie()))
                 
             }
-            let activity = Activity(id: 3, title: "Rowing", subtitle: "This week", amount: "\(count) minutes", image: "figure.rower")
+            let activity = Activity(id: 4, title: "Rowing", subtitle: "This week", amount: "\(count) minutes", image: "figure.rower")
             DispatchQueue.main.async{
                 self.activities["weekRowing"] = activity
+            }
+            let activityEnergy = Activity(id: 5, title: "Rowing", subtitle: "Calories this week", amount: "\(countEnergy) kcal", image: "figure.rower")
+            DispatchQueue.main.async{
+                self.activities["weekRowingEnergy"] = activityEnergy
+            }
+            
+        }
+        healthStore?.execute(query)
+    }
+    func fetchWeekCoreStats () {
+        let workout = HKSampleType.workoutType()
+        let timePredicate = HKQuery.predicateForSamples(withStart: .startOfWeek, end: Date())
+        let workoutPredicate = HKQuery.predicateForWorkouts(with: .coreTraining)
+        let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [timePredicate, workoutPredicate])
+        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 20, sortDescriptors: nil) { _, sample, error in
+            guard let workouts = sample as? [HKWorkout], error == nil else {
+                print("error fetching week core training data")
+                return
+            }
+            var count: Int = 0
+            var countEnergy: Int = 0
+            for workout in workouts {
+                let duration = Int(workout.duration)/60
+                count += duration
+                countEnergy += Int(workout.totalEnergyBurned!.doubleValue(for: .kilocalorie()))
+            }
+            let activity = Activity(id: 6, title: "Core training", subtitle: "This week", amount: "\(count) minutes", image: "figure.core.training")
+            DispatchQueue.main.async{
+                self.activities["weekCore"] = activity
+            }
+            let activityEnergy = Activity(id: 7, title: "Core training", subtitle: "Calories this week", amount: "\(countEnergy) kcal", image: "figure.core.training")
+            DispatchQueue.main.async{
+                self.activities["weekCoreEnergy"] = activityEnergy
             }
             
         }
