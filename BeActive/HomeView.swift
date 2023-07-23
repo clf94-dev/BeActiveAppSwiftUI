bf87662fb078427c0ef4b7300e98a02594a9c9ef
clf94-dev <clf94@hotmail.es>
2023-07-23 18:35:18 +0200

    - animate welcome title

--- BeActive/HomeView.swift
+++ BeActive/HomeView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var manager: HealthManager
+    let welcomeArray = ["Welcome", "Bienvenido", "Bienvenue"]
+    @State private var currentIndex = 0
     var body: some View {
         VStack (alignment: .leading){
-            Text("Welcome")
+            Text(welcomeArray[currentIndex])
                 .font(.largeTitle)
                 .padding()
+                .foregroundColor(.secondary)
+                .animation(.easeOut(duration: 1), value: currentIndex)
+                .onAppear{
+                    startWelcomeTimer()
+                }
             LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)) {
                 ForEach(manager.mockActivities.sorted(by: { $0.value.id <  $1.value.id} ), id: \.key){ item in
                     ActivityCard(activity: item.value)
@@ -22,6 +29,14 @@ struct HomeView: View {
             }.padding(.horizontal)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+    
+    func startWelcomeTimer() {
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            withAnimation {
+                currentIndex = (currentIndex + 1) % welcomeArray.count
+            }
+        }
     }
 }
 
